d7402e77f01a3a4b2a8d113eb91e046aadd83b67
kreafik <tasarim@cihanduran.com>
2021-12-01 17:24:20 +0300

    Yenilik

---
+++ Temeller Part 2/5 - Window Object/app.js
@@ -0,0 +1,54 @@
+// Alert
+
+// alert("Merhaba"); // Popup
+
+
+
+// console.log(cevap);
+
+// if (confirm("Hayırdır?")){
+//     console.log("Hayır hayır");
+// }
+// else {
+//     console.log("Değil değil");
+// }
+4
+
+// const cevap = prompt("2 + 2 = ?");
+
+// if (cevap == "4") {
+//     console.log("Doğru");
+// }
+// else {
+//     console.log("Yanlış");
+// }
+
+
+
+
+let value;
+
+value = window;
+value = window.location;
+value = window.location.host;
+value = window.location.hostname;
+value = window.location.port;
+value = window.location.href;
+
+// if (confirm("Sayfa yenilensin mi?")){
+//     window.location.reload();
+// }
+// else {
+//     confirm("Sayfa yenilenmedi!");
+// }
+
+value = window.outerHeight;
+value = window.outerWidth;
+value = window.innerHeight;
+value = window.innerWidth;
+value = window.outerWidth;
+
+value = window.scrollX;
+value = window.scrollY;
+
+console.log(value);
