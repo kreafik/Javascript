5d41d6348f37b6e504990a20e2853bc6ad519df7
kreafik <tasarim@cihanduran.com>
2021-12-01 14:07:23 +0300

    Yeni Çalışma

--- Temeller/Karşılaştırma Operatörler, Koşullar ve Ternary Operatör/app.js
+++ Temeller/Karşılaştırma Operatörler, Koşullar ve Ternary Operatör/app.js
@@ -1,5 +1,7 @@
 // Karşılaşmtırma Operatorleri
 
+// Eğer bir tarafta sayı bir tarafta string varsa, otomatik olarak string e dönüştürülür.
+
 // ==       İki değerin eşitliğini kontrol eder
 // ===      İki değerin hem eşitliğini hemde tipi kontrol eder
 // !=       Eşit değil midir operatörü, eşitse false, değilse true
@@ -36,7 +38,7 @@
 // Not Operatörü 
 // İşlem ne kadar doğru olursa olsun, false verir
 
-console.log(!(2 == 2));
+// console.log(!(2 == 2));
 
 // And Operatörü - 
 // Bütün işlemleri toplar her birini
@@ -44,9 +46,34 @@ console.log(!(2 == 2));
 // herhangi birisinin false olması durumunda
 // false verir
 
-console.log((2 == 2) && ("Rıza" == "Ahmet"))
+// console.log((2 == 2) && ("Rıza" == "Ahmet"))
 
 // Or Operatörü
 // Koşullardan herhangi birisi true olsa bile genel sonuç true çıkar
 
+// console.log((4 == 2) || ("Ahmet" == "Ahmet"));
 
+
+// Koşullar
+
+
+// if (kosul) {
+//     // İf in bloğu burası
+// }
+
+
+
+// let a = true;
+
+// if (a == true) {
+//     console.log("Başarılı!")
+// }
+
+
+const error = true;
+
+if (error == true) {
+    console.log("Hata oluştu!");
+}
+
+console.log("Deneme!");
