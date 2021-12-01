233dc5beaafd82d827ae2170ed9e6edae39cb016
kreafik <tasarim@cihanduran.com>
2021-12-01 14:56:14 +0300

    Yenilij

---
+++ Temeller/Fonksiyonlar, IIFE ve Anonim/app.js
@@ -0,0 +1,12 @@
+// Fonksiyon tanımlama
+
+
+function merhaba(name,age){
+    console.log(`İsim ${name} Yaş: ${age}`);
+}
+
+merhaba("Murat", 25); // Fonksion Çağrısı / Function Call
+
+merhaba("Ayşe", 30);
+
+merhaba();
