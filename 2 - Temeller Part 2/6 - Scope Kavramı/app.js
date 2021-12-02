7a5b3a1c631638c20cc13ca91b0342b7b45b1769
kreafik <tasarim@cihanduran.com>
2021-12-02 11:38:50 +0300

    Yeni Dosyalama

---
+++ 2 - Temeller Part 2/6 - Scope Kavramı/app.js
@@ -0,0 +1,52 @@
+// Scope Kavramı
+
+let a = "123456";
+
+
+
+// var value1 = 10;
+// let value2 = 20;
+// const value3 = 30;
+
+
+// // function Func(id){
+// //     var value1 = 40;
+// //     let value2 = 50;
+// //     const value3 = 60;
+
+// //     console.log(value1,value2,value3, id);
+// //     console.log(typeof id);
+
+// // }
+// // Func("Test");
+
+
+// if (true){
+//     var value1 = 40;
+//     let value2 = 50;
+//     const value3 = 60;
+
+//     console.log(value1,value2,value3);
+
+// }
+
+
+if(true){
+    let a = "1356";
+}
+
+console.log(a);
+
+// console.log(value1,value2,value3);
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+
+
+// Global Scope
+
--- Temeller Part 2/6 - Scope Kavramı/app.js
+++
@@ -1,9 +0,0 @@
-// Scope Kavramı
-
-var value = 10;
-let value2 = 20;
-const value3 = 30;
-
-
-// Global Scope
-
